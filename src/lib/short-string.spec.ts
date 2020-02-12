6e5c0a25e3497896c833de8cdbc3d7b046ba54c6
LucasPaganini <lucaspaganinidm@gmail.com>
2020-02-12 07:21:05 -0300

    Add more tests

--- src/lib/short-string.spec.ts
+++ src/lib/short-string.spec.ts
@@ -1,11 +1,11 @@
 import { ShortString } from './short-string.class'
 
 describe('ShortString', () => {
-
   it('Should trim the value', () => {
-    const actual = ShortString('   ').value
-    const expected = ''
-    expect(actual).toBe(expected)
+    const actuals = ['   ', ' abc ', ' a b c '].map(v => ShortString(v)).map(v => v.value)
+    const expecteds = ['', 'abc', 'a b c']
+
+    for (let i = 0; i < actuals.length; i++) expect(actuals[i]).toBe(expecteds[i])
   })
 
   it('Should wrap the string', () => {
@@ -15,8 +15,29 @@ describe('ShortString', () => {
   })
 
   it('Should reject long strings', () => {
-    const actual = () => ShortString(''.padEnd(300, 'abc')).value
-    expect(actual).toThrow()
+    const actuals = [0, 50, 200, 256, 257, 500, 1000, 2000]
+    const expecteds = [true, true, true, true, false, false, false, false]
+
+    for (let i = 0; i < actuals.length; i++) {
+      const str = ''.padEnd(actuals[i], 'abc')
+      const fn = () => ShortString(str)
+
+      const expectedToThrow = !expecteds[i]
+      if (expectedToThrow) expect(fn).toThrowError('Too long')
+      else expect(fn).not.toThrow()
+    }
   })
 
+  it('Should instantiate', () => {
+    const str = ''
+    const actuals = [ShortString(str), new ShortString(str)]
+    const actualEithers = [ShortString.fromRaw(str), ShortString.fromAny(str)]
+
+    actuals.forEach(v => expect(v).toBeInstanceOf(ShortString))
+    actualEithers.forEach(v => {
+      expect(v._tag).toBe('Right')
+      if (v._tag !== 'Right') throw Error()
+      expect(v.right).toBeInstanceOf(ShortString)
+    })
+  })
 })
