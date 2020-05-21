88eff321d87d9329b66e6f8613bdff26dd2293c2
Lucas Paganini <lucaspaganinidm@gmail.com>
2020-05-21 20:23:34 -0300

    Exit if some command fails

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Clean the last build
-rimraf dist dist-tsc
+rimraf dist dist-tsc &&
 
 # Build typescript files in dist-tsc/
-tsc
+tsc &&
 
 # Copy 
 # - non typescript files on src/ 
 # - the typescript build on dist-tsc/  
 # to dist/
-copyfiles --up=1 "dist-tsc/**/*" "src/**/!(*.ts)" dist
+copyfiles --up=1 "dist-tsc/**/*" "src/**/!(*.ts)" dist &&
 
 # Clean the typescript build
-rimraf dist-tsc
+rimraf dist-tsc &&
 
 # Improve types compatibility
 node scripts/improve-typescript-compatibility dist
